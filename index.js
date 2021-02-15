f5db2b44610f89e5147c6456b232019c59ff8a54
Faisal Nour <faisalskhalifa@gmail.com>
2021-02-14 23:49:19 -0500

    user input for manager and engineer have been implemented

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var inquirer = require('inquirer')
 
 
 class Employee {
@@ -28,7 +29,7 @@ class Employee {
 }
 
 class Manager extends Employee{
-    constructor(name,id, email,role, officNumber)
+    constructor(name,id, email, officNumber)
     {
         super(name, id, email, "Manager")
 
@@ -73,3 +74,97 @@ class Intern extends Employee{
     }
 }
 
+
+var employees = []
+
+async function main(){
+
+  const managerInfo = await inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Enter the manager name"
+        },
+        {
+            name: "id",
+            message: "Enter employee ID"
+        },
+        {
+            name: "email",
+            message: "enter employee's email"
+        },
+        {
+            name:"office",
+            message: "Enter office number"
+        },
+        {
+            name: "options",
+            type: "list",
+            message: "what do you want to do next?",
+            choices: ["Add engineer","Add intern", "Finish building my team"]
+        }
+    ])
+
+    employees.push(new Manager(managerInfo.name,managerInfo.id, managerInfo.email, managerInfo.office))
+
+
+    if(managerInfo.options == "Add engineer"){
+        getEngineerInfo()
+    }
+    if (managerInfo.options == "Finish building my team"){
+        console.log(employees)
+    }
+    // console.log(employees)
+}
+
+async function getEngineerInfo() {
+    let engineerInfo = await inquirer.prompt([
+        {
+            name: "name",
+            type: "input",
+            message: "Enter the engineer's name"
+        },
+        {
+            name: "id",
+            message: "Enter the engineer employee ID"
+        },
+        {
+            name: "email",
+            message: "enter the engineer's email"
+        },
+        {
+            name:"github",
+            message: "Enter the engineer's github account"
+        },
+        {
+            name: "options",
+            type: "list",
+            message: "what do you want to do next?",
+            choices: ["Add engineer","Add intern", "Finish building my team"]
+        }
+    ])
+
+    employees.push(new Engineer(engineerInfo.name, engineerInfo.id, engineerInfo.email, engineerInfo.github))
+
+    if(engineerInfo.options === "Finish building my team")
+    {
+        console.log(employees)
+    }
+}
+
+function displayManager(name,id, email, office){
+    let man = new Manager(name, id, email, office) 
+
+    console.log('Thanks for providing the information')
+
+    console.log(`Manager name: ${man.name} 
+    Manger id: ${man.id}
+    Manager email: ${man.email}
+    Manager role: ${man.role}
+    Manager office number: ${man.officNumber}`)
+}
+
+
+
+
+main()
